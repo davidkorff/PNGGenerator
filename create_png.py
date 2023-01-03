733d19f44f758dc1d3bb3881e7018d29e24468c6
Korff <dkorff@leedsworld.com>
2023-01-03 14:53:42 -0500

    percentage, number, and outlier logic to IMG

--- create_png.py
+++ create_png.py
@@ -28,12 +28,6 @@ for personalized_object in data['Template']['PersonalizedObjects']:
     obj_content = obj['ObjectContent']
     obj_location = obj['ObjectLocation']
 
-    # Get the object dimensions and offsets
-    obj_width = int(float(obj_location['ObjectWidth'])/pxToMillConv)
-    obj_height = int(float(obj_location['ObjectHeight'])/pxToMillConv)
-    obj_x_offset = int(float(obj_location['ObjectXOffset'])/pxToMillConv)
-    obj_y_offset = int(float(obj_location['ObjectYOffset'])/pxToMillConv)
-
     if obj_type == 'IMG':
         # Open the image file
         import requests
@@ -48,12 +42,95 @@ for personalized_object in data['Template']['PersonalizedObjects']:
         # Open the local image file
         image = Image.open('image.png')
 
+        # Get the width and height of the image
+        width, height = image.size
+
+        if 'ObjectWidth' in obj_location:
+            if '%' in obj_location['ObjectWidth']:
+                obj_width = int(float(obj_location['ObjectWidth'].strip('%'))/100 * template_width)
+                if 'ObjectHeight' in obj_location:
+                    if '%' in obj_location['ObjectHeight']:
+                        obj_height = int((float(obj_location['ObjectHeight'].strip('%')))/100 * template_height)
+                    elif str(obj_location['ObjectHeight']).digit():
+                        obj_height = int(float(obj_location['ObjectHeight'])/pxToMillConv)
+                    else:
+                        obj_height = int(height)
+                else:
+                    obj_height = int(float(obj_width/width)*height)
+            elif str(obj_location['ObjectWidth']).isdigit():
+                obj_width = int(float(obj_location['ObjectWidth'])/pxToMillConv)
+                if 'ObjectHeight' in obj_location:
+                    if '%' in obj_location['ObjectHeight']:
+                        obj_height = int((float(obj_location['Objectheight'].strip('%')))/100 * template_height)
+                    elif str(obj_location['ObjectHeight']).isdigit():
+                        obj_height = int(float(obj_location['ObjectHeight'])/pxToMillConv)
+                    else:
+                        obj_height = int(height)
+            else:
+                obj_width = int(width)
+                if 'ObjectHeight' in obj_location:
+                    if '%' in obj_location['ObjectHeight']:
+                        obj_height = int(float(obj_location['Objectheight'].strip('%'))/100 * template_height)
+                    elif str(obj_location['ObjectHeight']).isdigit():
+                        obj_height = int(float(obj_location['ObjectHeight'])/pxToMillConv)
+                    else:
+                        obj_height = int(height)
+        elif 'ObjectHeight' in obj_location:
+            if '%' in obj_location['ObjectHeight']:
+                obj_height = int(float(obj_location['ObjectHeight'].strip('%'))/100 * template_height)
+            elif str(obj_location['ObjectHeight']).isdigit():
+                obj_height = int(float(obj_location['ObjectHeight'])/pxToMillConv)
+            else:
+                obj_height = int(height)
+        else:
+            obj_height = int(height)
+            obj_width = int(width)
+
+
+
+
+        
+
+        # Print the width of the image
+        print(obj_height)
+        print(obj_width)
+
+        if 'ObjectXOffset' in obj_location:
+            if '%' in obj_location['ObjectXOffset']:
+                obj_x_offset = int((float((obj_location['ObjectXOffset'].strip('%')))/100 * template_width)-(obj_width/2))
+            elif str(obj_location['ObjectXOffset']).isdigit():
+                obj_x_offset = int(float(obj_location['ObjectXOffset'])/pxToMillConv)
+            else:
+                obj_x_offset = 0
+        else:
+            obj_x_offset = 0
+        if 'ObjectYOffset' in obj_location:
+            if '%' in obj_location['ObjectYOffset']:
+                obj_y_offset = int((float((obj_location['ObjectYOffset'].strip('%')))/100 * template_height)-(obj_height/2))
+            elif str(obj_location['ObjectYOffset']).isdigit():
+                obj_y_offset = int(float(obj_location['ObjectYOffset'])/pxToMillConv)
+            else:
+                obj_y_offset = 0
+        else:
+            obj_y_offset = 0
+
         # Resize the image to the specified dimensions
         image = image.resize((obj_width, obj_height), Image.ANTIALIAS)
 
         # Paste the image onto the template image at the specified location
         template_image.paste(image, (obj_x_offset, obj_y_offset, obj_x_offset + obj_width, obj_y_offset + obj_height))
+
+         
+
+
+
     elif obj_type == 'Text':
+        # Get the object dimensions
+        obj_width = int(float(obj_location['ObjectWidth'])/pxToMillConv)
+        obj_height = int(float(obj_location['ObjectHeight'])/pxToMillConv)
+        obj_x_offset = int(float(obj_location['ObjectXOffset'])/pxToMillConv)
+        obj_y_offset = int(float(obj_location['ObjectYOffset'])/pxToMillConv)
+
         # Load the TTF font file
         font_path = 'arial.ttf'
         font = ImageFont.truetype(font_path, int(float(obj_content['FontSize'])))
